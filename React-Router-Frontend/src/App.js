17f9a02c39ebd19a08ba882200aa274c761c61ac
Wilton Batiquin <cbasher24@gmail.com>
2025-10-17 13:21:35 +0800

    Add routing and page components for Home, About, and Contact

--- React-Router-Frontend/src/App.js
+++ React-Router-Frontend/src/App.js
@@ -1,16 +1,19 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
-createBrowserRouter([
-  { path: "/", },
-  { path: "/about", },
-  { path: "/contact", },
+const router = createBrowserRouter([
+  { path: "/", element: <Home />, },
+  { path: "/about", element: <About />, },
+  { path: "/contact", element: <Contact />, },
 ])
 
 
 function App() {
 
-  return <div>Hello</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
