76c08afbc99ad11d8a0ce040440bd720d603a0b4
Wilton Batiquin <cbasher24@gmail.com>
2025-10-17 13:10:59 +0800

    initial commmit

---
+++ React-Router-Frontend/src/App.js
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router-dom";
+
+
+createBrowserRouter([
+  { path: "/", },
+  { path: "/about", },
+  { path: "/contact", },
+])
+
+
+function App() {
+
+  return <div>Hello</div>;
+}
+
+export default App;
