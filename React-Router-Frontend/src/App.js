cc5e3b45921f85d23711cdb051b3fb20121adafc
Wilton Batiquin <cbasher24@gmail.com>
2025-10-20 17:40:37 +0800

    Layouts & Nested Routes

--- React-Router-Frontend/src/App.js
+++ React-Router-Frontend/src/App.js
@@ -4,12 +4,20 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import ProductsPage from "./pages/ProductsPage";
+import RootLayout from "./pages/Root";
 
 const router = createBrowserRouter([
-  { path: "/", element: <Home />, },
-  { path: "products", element: <ProductsPage />, },
-  { path: "/about", element: <About />, },
-  { path: "/contact", element: <Contact />, },
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <Home />, },
+      { path: "products", element: <ProductsPage />, },
+      { path: "/about", element: <About />, },
+      { path: "/contact", element: <Contact />, },
+    ]
+  },
+
 ]);
 
 
---
+++ React-Router-Frontend/src/components/MainNavigation.js
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+
+export default function MainNavigation() {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/products">Products</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
