e30da98b92678c279c192e6032df0f423044ceff
Wilton Batiquin <cbasher24@gmail.com>
2025-10-22 18:28:02 +0800

    Added useNavigation to give users feedback when fetching data

--- RouterMiester/frontend/src/pages/RootElement.js
+++ RouterMiester/frontend/src/pages/RootElement.js
@@ -1,11 +1,16 @@
-import MainNavigation from "./MainNavigation"
-import { Outlet } from "react-router-dom"
+import MainNavigation from "./MainNavigation";
+import { Outlet, useNavigation } from "react-router-dom";
+
 export default function RootElement() {
+
+    const navigation = useNavigation();
     return (
         <>
             <MainNavigation />
             <main className="content">
+                {navigation.state === 'loading' && <p>Loading...</p>}
                 <Outlet />
             </main>
-        </>)
+        </>
+    );
 }
